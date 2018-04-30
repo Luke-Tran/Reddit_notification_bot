54d4b4dcf102e9e7b89b71da69f858c229b1b6d0
Luke-Tran <luketran08@gmail.com>
2018-04-30 10:44:35 -0400

    Modified the batch file to allow rerunning the bot after the process has stopped

--- run_reddit_bot.bat
+++ run_reddit_bot.bat
@@ -2,5 +2,13 @@
 @echo off
 
 @REM The program is executed, it will run this command in the command line.
+:start_marker
 python reddit_notification_bot.py %*
-pause
+
+echo Process stopped. Close command prompt? (y/n)
+set/p restart=
+if %restart%==n goto start_marker
+if %restart%==y goto terminate_marker
+
+:terminate_marker
+exit
